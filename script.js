933daadee0983b65c38825ab172089ab5e90b4be
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-17 18:49:21 -0500

    Declare variables to make operations (Step #2).
    The variables are value1, value2 and operator.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-function add(val1, val2) {
+let value1, value2, operator;
+
+function add(value1, value2) {
 
 }
 
-function subtract(val1, val2) {
+function subtract(value1, value2) {
     
 }
 
-function multiply(val1, val2) {
+function multiply(value1, value2) {
     
 }
 
-function divide(val1, val2) {
+function divide(value1, value2) {
     
 }
