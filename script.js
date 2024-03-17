46c91a92e13575bb95d4d5b47f3f91506f52a0fe
cadltop <158861368+cadltop@users.noreply.github.com>
2024-03-17 18:57:42 -0500

    Create operate function.
    This functions calls the other functions depending on the value of the operate variable.

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ function multiply(value1, value2) {
 function divide(value1, value2) {
     
 }
+
+function operate(value1, value2, operator) {
+    switch (operator) {
+        case '+':
+            add(value1, value2);
+        case '-':
+            subtract(value1, value2);
+        case '*':
+            multiply(value1, value2);
+        case '/':
+            divide(value1, value2);
+    }
+}
